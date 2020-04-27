8b715c6c97025ef7dfef182867cf9bcbca92ad3c
Wilkin Shum <60490056+wilkinshum@users.noreply.github.com>
2020-04-27 16:47:54 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,18 @@
 class minecraft {
+    $url = 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar'
+    $install_dir = '/opt/minecraft'
     file { '/opt/minecraft':
         ensure => directory,
     }
-    file {'/opt/minecraft/server.jar':
+    file {"$(install_dir)/server.jar":
         ensure => file,
-        source => 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
+        source => $url,
+        before => Service['minecraft'],
     }
     package {'java':
         ensure => present,
     }
-    file {'/opt/minecraft/eula.txt':
+    file {"$(install_dir)/eula.txt":
         ensure => file,
         content => 'eula=true',
     }
@@ -20,5 +23,6 @@ class minecraft {
     service {'minecraft':
         ensure => running,
         enable => true,
+        require => [Package['java'],File["$(install_dir)/eula.txt"],File['/etc/systemd/system/minecraft.service']],
     }
 }
