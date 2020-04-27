409463b4005775714ae6e64209b48a5b4bb84eda
Wilkin Shum <60490056+wilkinshum@users.noreply.github.com>
2020-04-27 16:23:32 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
     }
     file {'/opt/minecraft/server.jar':
         ensure => file,
-        source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+        source => 'puppet:///modules/minecraft/server.jar',
     }
     package {'java':
         ensure => present,
