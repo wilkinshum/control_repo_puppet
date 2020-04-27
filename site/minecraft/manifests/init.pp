9aaa6455b88a2af275d17b9179c5135fe9fbe327
Wilkin Shum <60490056+wilkinshum@users.noreply.github.com>
2020-04-27 17:10:05 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft (
     $url = 'http://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar',
     $install_dir = '/opt/minecraft'
 ){
-    file { '/opt/minecraft':
+    file { "$(install_dir)":
         ensure => directory,
     }
     file {"$(install_dir)/server.jar":
