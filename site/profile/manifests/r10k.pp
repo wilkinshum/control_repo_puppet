3fd34d147de70e508a50df006e9e715871c49843
Wilkin Shum <60490056+wilkinshum@users.noreply.github.com>
2020-04-29 16:12:07 -0400

    production

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,14 +1,13 @@
 class profile::r10k {
-        class {'r10k':
-            remote => 'https://github.com/wilkinshum/control_repo_puppet',
-        }
-        class {'r10k::webhook::config':
-            use_mcollective => false,
-            enable_ssl      => false,
-        }
-        class {'r10k::webhook':
-            user    => 'root',
-            group   => '0',
-        }
+  class {'r10k':
+    remote => 'https://github.com/wilkinshum/control_repo_puppet',
+  }
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+  }
 }
-
